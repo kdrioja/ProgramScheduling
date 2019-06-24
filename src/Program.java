9c6472cfffb5c434b6a1ce500b86428859005239
Kenia Rioja <kdrioja@gmail.com>
2019-06-23 23:11:16 -0700

    Created Program class

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    int startTime;
+    int duration;
+
+    public Program(int startTime, int duration) {
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+}
