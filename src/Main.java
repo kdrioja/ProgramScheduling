4636971a3ea95897d09e5f8fb503cb88e4aaf6a3
Kenia Rioja <kdrioja@gmail.com>
2019-06-23 23:22:49 -0700

    Added interesting commentary from leetcode user about the use of a Tree

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     /**
@@ -8,9 +11,54 @@ public class Main {
      and 2nd is the execution time.
      */
 
+    /**
+     Understand:
+        - Will the start time ever be negative?
+        - Will the new programs list be sorted?
+        - Can I use extra space?
+
+     Match:
+        - Interval problems
+
+     Plan/Psudocode:
+        - First create the return list with the same size of
+          newPrograms
+        - for loop to go through all of the Program objects in
+          the newPrograms list
+            check if program fits into
+     */
+
+    /**
+     If we actually need to add and merge valid new programs,
+     I think that only a binary tree will work here (TreeMap in
+     java). So if we add a new program one by one, it will be
+     O(log n) complexity for each program. Using a regular array
+     wouldn't be good because you need to insert a program in the
+     middle and shift other elements to the right which will be
+     O(n), this is worse than O(log n).
+
+     For your example:
+     (18,7), you search the highest element lower than 18, it is
+     (10,5), then you search the lowest element higher than 18,
+     it is (25, 15).
+     Then you check if 10+5 <= 18 and 18+7 <= 25, it is true, then
+     you add this node to the binary tree (O(logn)) and return true.
+     (12,10), you search the highest element lower than 12, it is
+     (10,5), then you search the lowest element higher than 12, it
+     is (18, 7).
+     Then you check 10+5 <= 12 and 12+10 <= 18, it is false. Then
+     you just return false.
+     */
+
     public static void main(String[] args) {
         Program p1 = new Program(10, 5);
         Program p2 = new Program(25, 25);
+
+        List<Program> list1 = new ArrayList<>();
+        list1.add(p1);
+        list1.add(p2);
+
+
 
 
     }
