a0912e7884f69f4660c72b46a1a346e92bca722a
Kenia Rioja <kdrioja@gmail.com>
2019-06-23 23:14:35 -0700

    Added problem description

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+    /**
+     Given a sorted list of already scheduled programs and a
+     list of new programs, write an algorithm to find if the
+     given new programs can be scheduled or not? Each program
+     is a pair of values where 1st value is the starting time
+     and 2nd is the execution time.
+     */
+
     public static void main(String[] args) {
+        Program p1 = new Program(10, 5);
+        Program p2 = new Program(25, 25);
+
 
     }
 }
